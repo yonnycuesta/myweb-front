91e8159795da4686d5caad9f6fb116b37f61d693
yonnycuesta <yocumo1998@gmail.com>
2022-09-30 00:08:54 -0500

    Logout

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,5 +1,7 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import jwt_decode from 'jwt-decode';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +10,21 @@ import { Component, Inject, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(@Inject(DOCUMENT) private document: Document) { }
+  userData: any;
+  token: any;
+  constructor(@Inject(DOCUMENT) private document: Document, private router: Router) { }
 
   ngOnInit(): void {
+    this.token = localStorage.getItem('token');
+    this.userData = jwt_decode(this.token);
   }
 
   sidebarToggle(){
     this.document.body.classList.toggle('toggle-sidebar');
   }
 
+  logout(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { ICategory } from '../interfaces/category-interface';
+import { ICategory } from '../interfaces/category';
 
 @Injectable({
   providedIn: 'root',
